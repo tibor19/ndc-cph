540ce3e69657ae1a7f23d877e7c2d3f02d4ac796
Tiberiu Covaci <3577486+tibor19@users.noreply.github.com>
2022-06-02 14:59:19 +0200

    Some more changes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World, from Actions, with swapping slots!");
+app.MapGet("/", () => "Hello World, from Actions, with swapping slots, and fixes!");
 
 app.Run();
